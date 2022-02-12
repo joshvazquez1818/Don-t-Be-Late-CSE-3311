cc47b153a4f63d43eda5885178fe255ca24965a7
Alexander-Rios <AlexanderRiosCS@gmail.com>
2022-02-12 04:24:03 -0600

    Reverted all the way back & fixed gitignore. -Alex

---
+++ DontBeLate/Assets/TitleToGame.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleToGame : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
