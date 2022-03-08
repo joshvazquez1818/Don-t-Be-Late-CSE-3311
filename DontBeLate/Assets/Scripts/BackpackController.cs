46684191663c2c61cf43f631ea61f7de2b78e306
Alexander-Rios <AlexanderRiosCS@gmail.com>
2022-03-07 21:22:48 -0600

    Final iteration 2 version

--- DontBeLate/Assets/Scripts/BackpackController.cs
+++ DontBeLate/Assets/Scripts/BackpackController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class BackpackController : MonoBehaviour
 {   
-    public Scene scene = SceneManager.GetActiveScene();
+    public Scene scene;
+    public void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
@@ -12,7 +16,11 @@ public class BackpackController : MonoBehaviour
             Destroy(gameObject);
             if(scene.name == "Level_1_1")
             {
-                SceneManager.LoadScene("Level_1_2");
+                SceneManager.LoadScene(4);
+            }
+            else if(scene.name == "Level_1_2")
+            {
+                SceneManager.LoadScene(2);
             }
         }
     }
